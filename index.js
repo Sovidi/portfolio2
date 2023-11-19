63aaa545c40273682c7ac62aa36ee106b3bb1780
Sovidi <lim132445@gmail.com>
2023-11-19 21:29:53 +0900

    sefsfsef

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 var cors= require('cors')
 var bodyParser = require('body-parser')
-require('dotenv').config();
+// require('dotenv').config();
 
 app.use(express.static(`dist`));
 
