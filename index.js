a4c2a9498f94251453aa7bd27eb2584ed8b1fd17
Sovidi <lim132445@gmail.com>
2023-12-11 02:17:54 +0900

    servertest

--- index.js
+++ index.js
@@ -12,45 +12,50 @@ app.use(bodyParser.json());
 
 const dbConnect = async (str) => {
     const { MongoClient } = require('mongodb');
-    const client = await MongoClient.connect(process.env.S_HOST);
-    const db = client.db("portfolio")
-    const collection = db.collection(str)
-    // console.log(collection)
-    return collection
-}
+    const client = new MongoClient(process.env.S_HOST);
+    const db = client.db("portfolio");
+    const collection = db.collection(str);
+    console.log("접속성공");
 
-app.get('/api', async function (req, res) {
-    const collection = await dbConnect("comment"); 
-    let data = await collection.find().toArray();
+    return {client, collection};
+};
 
-    res.send(data);
-})
+// app.get('/api', async function (req, res) {
+//     const {client, collection} = await dbConnect("comment"); 
+//     let data = await collection.find().toArray();
+
+//     await client.close();
+//     res.send(data);
+// })
 
 app.post(`/api/insert`, async function(req, res) {
-    const collection = await dbConnect("comment"); 
+    const {client, collection} = await dbConnect("comment"); 
     await collection.insertOne(req.body);
     // const dataGet = await collection.find().toArray();
 
+    await client.close();
     res.send("글쓰여짐");
-})
+});
 
 app.put(`/api/put`, async function(req, res) {
-    const collection = await dbConnect("comment");
+    const {client, collection} = await dbConnect("comment");
     const qData = await req.body;
     await collection.updateOne({name: qData.name, email: qData.email}, {$set:{text: qData.text}});
     // const dataGet = await collection.find().toArray();
 
+    await client.close();
     res.send("글수정됨");
-})
+});
 
 app.delete(`/api/delete`, async function(req, res) {
-    const collection = await dbConnect("comment");
-    const qData = req.query;
+    const {client, collection} = await dbConnect("comment");
+    const qData = await req.query;
     await collection.deleteOne({name: qData.name, email: qData.email});
 
+    await client.close();
     res.send("지워짐");
-})
+});
 
 
 
-app.listen(3050)
+app.listen(3050);
